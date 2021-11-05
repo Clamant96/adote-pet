392f7ed4975de9f7df65f14bd58f4942e804474b
Clamant96 <kevin.lazzarotto@hotmail.com>
2021-11-04 23:31:11 -0300

    Estilizacao do modal para compor os dados de uma determinada postagem por meio de seu ID

--- app/Views/page/home.php
+++ app/Views/page/home.php
@@ -11,7 +11,7 @@
         <div class="animal-doacao">
             <div id="modalAnimal" class="container">
                 <div class="img">
-                    <img src="https://cdn.amigonaosecompra.com.br/1176x0/9c04f6ea-621a-4ba0-b97d-0161e0659730/47e56845-b1c6-4798-9ae0-1b25eda14347/47e56845-b1c6-4798-9ae0-1b25eda14347.jpeg?v=63803108619" alt="">
+                    <img src="https://cdn.amigonaosecompra.com.br/1176x0/9c04f6ea-621a-4ba0-b97d-0161e0659730/47e56845-b1c6-4798-9ae0-1b25eda14347/47e56845-b1c6-4798-9ae0-1b25eda14347.jpeg?v=63803108619" alt="Frufru linda">
                 </div>
                 <div class="conteudo">
                     <h2>Frufru linda</h2>
@@ -28,7 +28,34 @@
     <!-- CONTEUDO DO MODAL -->
     <div class="modal-content">
         <span class="close">&times;</span>
-        <p>Some text in the Modal..</p>
+        <div class="animal-focus">
+            <div class="img" style="background-image: url(https://cdn.amigonaosecompra.com.br/1176x0/9c04f6ea-621a-4ba0-b97d-0161e0659730/47e56845-b1c6-4798-9ae0-1b25eda14347/47e56845-b1c6-4798-9ae0-1b25eda14347.jpeg?v=63803108619);">
+            </div>
+            <div class="dados-animal">
+                <div class="nome-e-categoria-animal">
+                    <h2>Frufru linda</h2>
+                    <p>Cachorro | Macho | Filhote | Porte pequeno</p>
+                </div>
+                <div class="endereco-publicado-por">
+                    <div class="dados">
+                        <div class="icone">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
+                                <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
+                            </svg>
+                        </div>
+                        <p>Está em São Paulo, São Paulo</p>
+                    </div>
+                    <div class="dados">
+                        <div class="icone">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+                                <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                            </svg>
+                        </div>
+                        <p>Publicado por <a href="#">Maria Lucia Lopes</a> em 04/11/2021</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 
 </div>
